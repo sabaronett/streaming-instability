f54c98ed252512ecfe7d1192d5a39768de7bd9e8
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-04-02 08:29:59 -0700

    Automate post-run output organization

---
+++ organize.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "***START ORGANIZATION***"
+mkdir rst
+mkdir xdmf
+mkdir athdf
+mv -v *.pbs pbs/
+mv -v *.rst rst/
+mv -v *.xdmf xdmf/
+mv -v *.athdf athdf/
+echo "***END ORGANIZATION***"
