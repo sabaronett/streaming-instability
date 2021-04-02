d9edcfb43eb137158a7b348e4710cc20f8ab43b3
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-04-02 11:13:45 -0700

    Fix athinput path

--- linear/LinA/PLM/np4/run01/scripts/rhop_mov.py
+++ linear/LinA/PLM/np4/run01/scripts/rhop_mov.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 
 # Collect .athdf outputs, init sim consts. and grid
-athinput = athena_read.athinput('../athinput.si.nas')
+athinput = athena_read.athinput('../athinput.si')
 outputs = sorted(list(Path('../athdf').glob(athinput["job"]["problem_id"] +
                                         '.out1.*.athdf')))
 c_s = athinput['hydro']['iso_sound_speed'] # sound speed
