01a315cfa2f11643c23124aac9bc5cab37be3d6e
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-08-31 14:40:09 -0700

    Collect run output for 1-cmd sup shiftc

--- nonlinear/Pi/AB/0.1/organize.sh
+++ nonlinear/Pi/AB/0.1/organize.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "***START ORGANIZATION***"
+mkdir athdf
+mkdir output
 mkdir rst
 mkdir xdmf
-mkdir athdf
+mv -v *.athdf athdf/
+mv -v *.o* output/
+mv -v SI.hst output/
 mv -v *.rst rst/
 mv -v *.xdmf xdmf/
-mv -v *.athdf athdf/
 echo "***END ORGANIZATION***"
