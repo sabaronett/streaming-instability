62f7f97a4208e7353d8ab37b01627c7f89ad2d07
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-08-31 15:20:48 -0700

    Update organization scripts

--- nonlinear/Pi/AB/0.01-1024/organize.sh
+++ nonlinear/Pi/AB/0.01-1024/organize.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 echo "***START ORGANIZATION***"
-mkdir rst
-mkdir xdmf
 mkdir athdf
+mkdir output
+mkdir rst
+mkdir video
+mkdir xdmf
+mv -v *.athdf athdf/
+mv -v *.o* output/
+mv -v SI.hst output/
 mv -v *.rst rst/
 mv -v *.xdmf xdmf/
-mv -v *.athdf athdf/
 echo "***END ORGANIZATION***"
