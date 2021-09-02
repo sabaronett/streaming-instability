b6c0624eed42fec453a8410a40493ed9fc9338d5
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-09-02 08:10:51 -0700

    Fix order: .xdmf were moving to output/

--- nonlinear/Pi/BA/0.01-1024/organize.sh
+++ nonlinear/Pi/BA/0.01-1024/organize.sh
@@ -7,8 +7,8 @@ mkdir rst
 mkdir video
 mkdir xdmf
 mv -v *.athdf athdf/
-mv -v *.o* output/
-mv -v SI.hst output/
-mv -v *.rst rst/
 mv -v *.xdmf xdmf/
+mv -v *.rst rst/
+mv -v SI.hst output/
+mv -v *.o* output/
 echo "***END ORGANIZATION***"
