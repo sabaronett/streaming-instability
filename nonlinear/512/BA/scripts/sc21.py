13d7842dce0a6ad58c5da4d38884ffd586737dd2
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-09-13 13:04:22 -0700

    Test 4

--- nonlinear/512/BA/scripts/sc21.py
+++ nonlinear/512/BA/scripts/sc21.py
@@ -43,7 +43,8 @@ ax.set(aspect='equal', xticks=[], yticks=[], frame_on=False)
 ax.text(-.9, -.9, '{:.2f} years'.format(times[0]), color='white')
 img = ax.pcolormesh(xf, zf, clipped/epsilon, cmap='afmhot',
                     norm=colors.LogNorm(vmin/epsilon, vmax/epsilon))
-fig.tight_layout()
+fig.tight_layout(pad=0)
+fig.patch.set_facecolor('black')
 
 def animate(i):
     """Update frame.
@@ -62,4 +63,4 @@ anim = animation.FuncAnimation(fig, animate, frames=len(times), repeat=False)
 metadata = dict(title='Dust Density', artist='Stanley A. Baronett')
 plt.rcParams['animation.ffmpeg_path']='/nasa/pkgsrc/sles12/2018Q3/bin/ffmpeg3'
 writer = animation.FFMpegWriter(fps=30, metadata=metadata, bitrate=14500)
-anim.save('../video/sc21_test3.mp4', writer=writer)
+anim.save('../video/sc21_test4.mp4', writer=writer)
