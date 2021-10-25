a0d6d84609fd1e2e94f5661f5782d73c494d82a7
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-10-25 12:39:57 -0700

    100 stack

--- nonlinear/Pi/BA/0.02-1024/scripts/acs.py
+++ nonlinear/Pi/BA/0.02-1024/scripts/acs.py
@@ -12,7 +12,7 @@ from scipy import fftpack
 # Collect .athdf outputs, init sim consts.
 athinput = athena_read.athinput('../athinput.si')
 outputs = sorted(list(Path('../athdf').glob(athinput['job']['problem_id'] +
-                                            '.out2.019[5-9]*.athdf')))
+                                            '.out2.019*.athdf')))
 data = athena_read.athdf(outputs[0])
 stack = np.zeros_like(data['rhop'][0])
 
