0d8190235f71d5573974dbb28aa3aa5401115f4b
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-10-25 11:47:11 -0700

    NAS runs

---
+++ nonlinear/Pi/BA/0.10-1024/scripts/acs.py
@@ -0,0 +1,25 @@
+"""Dust Density Autocorrelation Stack (ACS)
+
+Compute autocorrelation of dust density and sum over multiple snapshots.
+"""
+import sys
+sys.path.insert(0, '/home6/sbaronet/athena-dust/vis/python')
+import athena_read
+import numpy as np
+from pathlib import Path
+from scipy import fft
+
+# Collect .athdf outputs, init sim consts.
+athinput = athena_read.athinput('../athinput.si')
+outputs = sorted(list(Path('../athdf').glob(athinput['job']['problem_id'] +
+                                            '.out2.019[5-9]*.athdf')))
+data = athena_read.athdf(outputs[0])
+stack = np.zeros_like(data['rhop'][0])
+
+for output in outputs:
+    dataFT = fft.fft2(data['rhop'][0])
+    dataAC = fft.ifft2(dataFT*np.conjugate(dataFT)).real
+    shifted = fft.fftshift(dataAC)
+    stack = np.add(stack, shifted)
+
+np.savez_compressed('autocorrelation/acs-{:d}'.format(len(outputs)),acs=stack)
