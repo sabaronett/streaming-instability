33d01eda8a0848f9952bce51f4d5e6a1f64ec094
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-01-31 07:18:07 -0800

    Organize .dat particle output

--- nonlinear/Pi/BA/0.05-2048/organize.sh
+++ nonlinear/Pi/BA/0.05-2048/organize.sh
@@ -2,11 +2,13 @@
 
 echo "***START ORGANIZATION***"
 mkdir athdf
+mkdir dat
 mkdir output
 mkdir rst
 mkdir video
 mkdir xdmf
 mv -v *.athdf athdf/
+mv -v *.dat athdf/
 mv -v *.xdmf xdmf/
 mv -v SI.0*.rst rst/
 cp -v SI.final.rst rst/
