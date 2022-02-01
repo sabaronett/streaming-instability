29fc409fe785831d2b49e64c51150a3f9f27329e
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-01-31 19:20:04 -0800

    Init full part. diff. tst rn for diag

---
+++ nonlinear/Pi/BA/0.05-64/organize.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "***START ORGANIZATION***"
+mkdir athdf
+mkdir dat
+mkdir output
+mkdir rst
+mkdir video
+mkdir xdmf
+mv -v *.athdf athdf/
+mv -v *.dat dat/
+mv -v *.xdmf xdmf/
+mv -v SI.0*.rst rst/
+cp -v SI.final.rst rst/
+cp -v SI.hst output/
+mv -v *.o* output/
+echo "***END ORGANIZATION***"
