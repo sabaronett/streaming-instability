4cac7da96360e94879e35c6f64a5e15f574109c0
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-02-08 11:41:09 -0800

    Include core dumps

--- nonlinear/Pi/AB/0.01/2048/organize.sh
+++ nonlinear/Pi/AB/0.01/2048/organize.sh
@@ -2,12 +2,14 @@
 
 echo "***START ORGANIZATION***"
 mkdir athdf
+mkdir core
 mkdir dat
 mkdir output
 mkdir rst
 mkdir video
 mkdir xdmf
 mv -v *.athdf athdf/
+mv -v core.* core/
 mv -v *.dat dat/
 mv -v *.xdmf xdmf/
 mv -v SI.0*.rst rst/
