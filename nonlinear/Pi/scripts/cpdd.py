35241e4fc442b5ffd5241630c0989b6b4531f3ca
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-02-10 11:31:35 -0800

    Testing PBS job working directory

--- nonlinear/Pi/scripts/cpdd.py
+++ nonlinear/Pi/scripts/cpdd.py
@@ -17,15 +17,15 @@ import numpy as np
 from pathlib import Path
 
 # Passed arguments
-path = sys.argv[1]                           # Relative path to run
+# path = sys.argv[1]                           # Relative path to run
 t_sat = float(sys.argv[2])                   # / T
 
 # Collect .athdf inputs, outputs, sim consts.
-athinput = athena_read.athinput(path+'/athinput.si')
+athinput = athena_read.athinput('../athinput.si')
 epsilon = athinput['problem']['epsilon']     # avg. BG dust/gas ρ-ratio
 dt = athinput['output2']['dt']               # time between rhop outputs
 i_sat = int(t_sat / dt)                      # output index of sat. state
-outputs = sorted(list(Path(path+'/athdf').glob(athinput["job"]["problem_id"]+
+outputs = sorted(list(Path('../athdf').glob(athinput["job"]["problem_id"]+
                                                '.out2.*.athdf')))
 rhops = []                                   # sorted dust density snapshots
 
@@ -35,7 +35,8 @@ for output in sat_outputs:
     data = athena_read.athdf(output)
     temp = data['rhop'].flatten() / epsilon  # flatten & convert
     rhops.append(np.sort(temp))              # sort
-    print('{:3.1f}%% done.'.format(100*len(rhops)/len(sat_outputs)))
+    prog = 100*len(rhops)/len(sat_outputs)   # percentage done
+    print('{:3.1f}%% done.'.format(prog), flush=True)
 
 # Find min., max., avg. of each ordered rhop over saturated state
 mins = np.amin(rhops, axis=0)
@@ -47,5 +48,5 @@ lnrhops = np.log(rhops)
 stds = np.exp(np.std(lnrhops, axis=0))
 cdf = np.linspace(1, 0 , mins.size, endpoint=False)
 
-np.savez_compressed(path+'/output/cpdd', cdf=cdf, mins=mins, maxs=maxs,
+np.savez_compressed('../output/cpdd', cdf=cdf, mins=mins, maxs=maxs,
                     avgs=avgs, stds=stds)
