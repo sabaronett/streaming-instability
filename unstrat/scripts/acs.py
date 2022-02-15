ee2d9d74114807d60493cfc25060b7cf8d78c557
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-02-15 14:04:49 -0800

    Add outputp block to hotfix fatal restart error

---
+++ unstrat/scripts/acs.py
@@ -0,0 +1,25 @@
+"""Dust Density Autocorrelation Stack (ACS)
+
+Compute autocorrelation of dust density and sum over multiple snapshots.
+"""
+import sys
+sys.path.insert(0, '/home6/sbaronet/athena-dust/vis/python')
+import athena_read
+import numpy as np
+from pathlib import Path
+from scipy import fftpack
+
+# Collect .athdf outputs, init sim consts.
+athinput = athena_read.athinput('../athinput.si')
+outputs = sorted(list(Path('../athdf').glob(athinput['job']['problem_id'] +
+                                            '.out2.01[8-9]*.athdf')))
+data = athena_read.athdf(outputs[0])
+stack = np.zeros_like(data['rhop'][0])
+
+for output in outputs:
+    dataFT = fftpack.fft2(data['rhop'][0])
+    dataAC = fftpack.ifft2(dataFT*np.conjugate(dataFT)).real
+    shifted = fftpack.fftshift(dataAC)
+    stack = np.add(stack, shifted)
+
+np.savez_compressed('../output/acs-{:d}'.format(len(outputs)),acs=stack)
