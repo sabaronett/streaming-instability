4f8ade0517f56ea22e4c96ee5549c88e934020e1
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-03-06 20:43:04 -0800

    NAS test run

--- unstrat/scripts/vpxd.py
+++ unstrat/scripts/vpxd.py
@@ -2,8 +2,8 @@
 #==============================================================================
 # vpxd.py
 #
-# Compiles and saves the flattened, zonal radial velocities and particle
-# densities of the dust during the saturated, turbulent state of the SI.
+# Plots the density-weighted, zonal radial velocity distributions of the dust
+# during the saturated, turbulent state of the SI across multiple runs.
 #
 # Author: Stanley A. Baronett
 # Created: 2022-03-02
@@ -12,23 +12,76 @@
 import sys
 sys.path.insert(0, '/home6/sbaronet/athena-dust/vis/python')
 import athena_read
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from pathlib import Path
 
-t_sat = float(sys.argv[1])     # [T]
-athinput = athena_read.athinput('athinput.si')
-dt = athinput['output1']['dt'] # time between vp1 outputs
-i_sat = int(t_sat/dt)          # output index of saturated state
-outputs = sorted(list(Path('athdf').glob(athinput["job"]["problem_id"]+
-                                         '.out1.*.athdf')))
-sat_outputs = outputs[i_sat:]  # slice saturated state
-vp1s, rhops = [], []
+fig, axs = plt.subplots(2, 1, figsize=(6,8), dpi=300)
+runs = ['AB', 'BA']
+Pis = [['0.01', 'tab:blue'], ['0.02', 'tab:green'],
+       ['0.05', 'tab:orange'], ['0.10', 'tab:red']]
+res = '2048'
+xlims = [(-1.0, 1.0), (-2.0, 2.0)]
+n_bins = 50
+arrays, vpx0s, avgvpxs, oldvpxs = [[], []], [], [], []
+t_sats = [2.0, 20.0]                                                 # from DDD
 
-for i,output in enumerate(sat_outputs):
-    data = athena_read.athdf(output)
-    vp1s = np.append(vp1s, data['vp1'].flatten())
-    rhops = np.append(rhops, data['rhop'].flatten())
+for i,ax in enumerate(axs.flat):
+    for Pi in Pis:
+        path = '../../%s/%s/%s/'%(runs[i], Pi[0], res)
+        print(f'Processing '+path+'...')
+        athinput = athena_read.athinput(path+'athinput.si')
+        i_sat = int(t_sats[i]/athinput['output3']['dt'])
+        if runs[i]=='AB' and Pi[0]=='0.01':
+            i_sat = int(4.0/athinput['output3']['dt'])
+        etav_K = float(Pi[0])*athinput['hydro']['iso_sound_speed']
+        hst = athena_read.hst(path+'output/SI.hst')
+        Np = athinput['problem']['npx1']*athinput['problem']['npx2']\
+             *athinput['problem']['npx3']
+        vpx0 = hst['vp1'][0]/Np/etav_K
+        oldvpx = np.average(hst['vp1'][i_sat:])/Np/etav_K
+        outputs = sorted(list(Path(path+'athdf').glob(athinput["job"]["problem_id"]+
+                                                      '.out1.*.athdf')))
+        sat_outputs = outputs[i_sat:]
+        vpxs, rhops = [], []
 
-    print(f'{i/len(sat_outputs):.1%} done.')
+        for j,output in enumerate(sat_outputs):
+            data = athena_read.athdf(output)
+            vpxs = np.append(vpxs, data['vp1'].flatten())
+            rhops = np.append(rhops, data['rhop'].flatten())
+            print(f'  {j/len(sat_outputs):.1%} done.')
 
-np.savez_compressed('output/vpxd', vp1s=vp1s, rhops=rhops)
+        arrays[0].append(runs[i])
+        arrays[1].append(Pi[0])
+        vpx0s.append(vpx0)
+        avgvpxs.append(np.average(vpxs, weights=rhops)/etav_K)
+        oldvpxs.append(oldvpx)
+        ax.hist(vpxs/etav_K, bins=n_bins, density=True, weights=rhops,
+                histtype='step', label=Pi[0])
+
+    ax.axvline(vpx0, c='black', ls='--', label=r'$v_{\mathrm{p},x,0}$')
+    ax.grid()
+    ax.legend(loc='upper right', title=r'$\Pi$')
+    ax.minorticks_on()
+    ax.set(xlim=xlims[i], ylim=(0, 2.0),
+           ylabel=r'd$f_\mathrm{zone}$ / d$v_{\mathrm{p},x}$')
+    ax.text(0.03, 0.97, runs[i], ha='left', va='top', size='xx-large',
+            transform=ax.transAxes)
+    ax.tick_params(which='both', top=True, right=True)
+
+ax.set(xlabel=r'$v_{\mathrm{p},x}$ / $(\eta v_\mathrm{K})$')
+
+tuples = list(zip(*arrays))
+names = ['Case', '$\Pi$']
+index = pd.MultiIndex.from_tuples(tuples, names=names)
+df = pd.DataFrame({
+     r'$v_{\textrm{p},x,0}$'   : vpx0s,
+     r'$v_{\textrm{p},x}$'     : avgvpxs,
+     r'Old $v_{\textrm{p},x}$' : oldvpxs},
+     index=index)
+s = df.style.format({
+    r'$v_{\textrm{p},x,0}$'   : '{:.3f}',
+    r'$v_{\textrm{p},x}$'     : '{:.3f}',
+    r'Old $v_{\textrm{p},x}$' : '{:.3f}'})
+s.to_latex(buf='vpxd.txt')
