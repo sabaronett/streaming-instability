42c40ad215b86dbbfac24d29fd28def50d5862ba
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-03-07 19:51:52 -0800

    Update script; try w/ last 2 outputs

--- unstrat/scripts/vpxd_mini.py
+++ unstrat/scripts/vpxd_mini.py
@@ -17,6 +17,9 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
+# Passed arguments
+last = float(sys.argv[1])                     # only process last many outputs
+
 fig, axs = plt.subplots(2, 1, figsize=(6,8), dpi=300)
 runs = ['AB', 'BA']
 Pis = [['0.01', 'tab:blue'], ['0.02', 'tab:green'],
@@ -24,9 +27,8 @@ Pis = [['0.01', 'tab:blue'], ['0.02', 'tab:green'],
 res = '2048'
 xlims = [(-1.0, 1.0), (-2.0, 2.0)]
 n_bins = 50
-arrays, vpx0s, avgvpxs, oldvpxs = [[], []], [], [], []
+arrays, avgvpxs = [[], []], []
 t_sats = [2.0, 20.0]
-last = 1                                       # only process last many outputs
 
 for i,ax in enumerate(axs.flat):
     for Pi in Pis:
@@ -36,51 +38,48 @@ for i,ax in enumerate(axs.flat):
         i_sat = int(t_sats[i]/athinput['output1']['dt'])
         if runs[i]=='AB' and Pi[0]=='0.01':
             i_sat = int(4.0/athinput['output1']['dt'])
-        print('  i_sat = ', i_sat)
         etav_K = float(Pi[0])*athinput['hydro']['iso_sound_speed']
         hst = athena_read.hst(path+'output/SI.hst')
         Np = athinput['problem']['npx1']*athinput['problem']['npx2']\
              *athinput['problem']['npx3']
         vpx0 = hst['vp1'][0]/Np/etav_K
-        hst_i_sat = int(t_sats[i]/athinput['output3']['dt'])
-        oldvpx = np.average(hst['vp1'][hst_i_sat:])/Np/etav_K
         outputs = sorted(list(Path(path+'athdf').glob(
                   athinput["job"]["problem_id"]+'.out1.*.athdf')))
-        print('  {:n} total outputs.'.format(len(outputs)))
         sat_outputs = outputs[-last:]
+        data = athena_read.athdf(sat_outputs[0])
+        t = data['Time']
+        label = r'{:s}, {:.1f} $T$'.format(Pi[0], t)
+        if runs[i]=='BA':
+            label = r'{:s}, {:.0f} $T$'.format(Pi[0], t)
         vpxs, rhops = [], []
-        print('  {:n} outputs to process.'.format(len(sat_outputs)))
 
         for j,output in enumerate(sat_outputs):
             data = athena_read.athdf(output)
             vpxs = np.append(vpxs, data['vp1'].flatten())
             rhops = np.append(rhops, data['rhop'].flatten())
-            print('    {:.0%} done.'.format(j/len(sat_outputs)))
+            print('  {:.0%} done'.format(j/len(sat_outputs)))
 
+        print('  Done.')
         arrays[0].append(runs[i])
         arrays[1].append(Pi[0])
-        vpx0s.append(vpx0)
         avgvpxs.append(np.average(vpxs, weights=rhops)/etav_K)
-        oldvpxs.append(oldvpx)
         ax.hist(vpxs/etav_K, bins=n_bins, density=True, weights=rhops,
-                histtype='step', label=Pi[0])
+                histtype='step', label=label)
 
-    ax.axvline(vpx0, c='black', ls='--', label=r'$v_{\mathrm{p},x,0}$')
+    ax.axvline(vpx0, c='black', ls='--', label=r'$v_{\mathrm{p},x,0},\,0\,T$')
     ax.grid()
-    ax.legend(loc='upper right', title=r'$\Pi$')
+    ax.legend(loc='upper right', title=r'$\Pi,\,t\geq$')
     ax.minorticks_on()
-    ax.set(xlim=xlims[i], ylim=(0, 2.0),
-           ylabel=r'd$f_\mathrm{zone}$ / d$v_{\mathrm{p},x}$')
+    ax.set(xlim=xlims[i], ylabel=r'd$f_\mathrm{zone}$ / d$v_{\mathrm{p},x}$')
     ax.text(0.03, 0.97, runs[i], ha='left', va='top', size='xx-large',
             transform=ax.transAxes)
     ax.tick_params(which='both', top=True, right=True)
 
 ax.set(xlabel=r'$v_{\mathrm{p},x}$ / $(\eta v_\mathrm{K})$')
-plt.savefig('scripts/figs/vpxd.pdf', bbox_inches='tight', pad_inches=0.01)
+plt.savefig(f'scripts/figs/vpxd_mini_{last:02}.pdf', bbox_inches='tight', pad_inches=0.01)
 
 tuples = list(zip(*arrays))
 names = ['Case', '$\Pi$']
 index = pd.MultiIndex.from_tuples(tuples, names=names)
-pd.DataFrame({r'$v_{\textrm{p},x,0}$'   : vpx0s,
-              r'$v_{\textrm{p},x}$'     : avgvpxs,
-              r'Old $v_{\textrm{p},x}$' : oldvpxs}, index=index)
+df = pd.DataFrame({r'$\langle v_{\textrm{p},x} \rangle$':avgvpxs}, index=index)
+print('=====TABLE=====\n', pd)
