c7ad264fe53c161f45daa26473ebcec2c32caa69
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-03-09 18:37:03 -0800

    hist auto flattens; comp skew/kurt over whole arr

--- unstrat/scripts/vpxd_hist.py
+++ unstrat/scripts/vpxd_hist.py
@@ -2,9 +2,9 @@
 #==============================================================================
 # vpxd_hist.py
 #
-# Computes and saves the probability density function histogram and standard
-# moments of zonal radial velocities, weighted by particle densities, of the
-# dust during the saturated, turbulent state of the SI.
+# Computes and saves the probability density function histogram and central
+# moments of the zonal radial velocity distribution, weighted by particle
+# densities, of the dust during the saturated, turbulent state of the SI.
 #
 # Author: Stanley A. Baronett
 # Created: 2022-03-09
@@ -14,8 +14,8 @@ import sys
 sys.path.insert(0, '/home6/sbaronet/athena-dust/vis/python')
 import athena_read
 import numpy as np
-from scipy.stats import skew, kurtosis
 from pathlib import Path
+from scipy.stats import skew, kurtosis
 
 t_sat, n_bins = float(sys.argv[1]), int(sys.argv[2]) # t_sat in code unit [T]
 athinput = athena_read.athinput('athinput.si')
@@ -30,17 +30,17 @@ print(f'Compiling data...', flush=True)
 
 for i,output in enumerate(sat_outputs):
     data = athena_read.athdf(output)
-    temp = data['vp1'].flatten()
-    vpxs.append(temp)
-    temp = data['rhop'].flatten()
-    rhops.append(temp)
+    vpxs.append(data['vp1'])
+    rhops.append(data['rhop'])
     print('  {:.0%}'.format(i/len(sat_outputs)), flush=True)
 
 print('  100%\nComputing histogram and central moments...', flush=True)
 hist, bin_edges = np.histogram(vpxs, bins=n_bins, weights=rhops, density=True)
 wavg = np.average(vpxs, weights=rhops)
 wdist = vpxs*rhops
-wvar, wskew, wkurt = np.var(wdist), skew(wdist), kurtosis(wdist)
+wvar = np.var(wdist)
+wskew = skew(wdist, axis=None)
+wkurt = kurtosis(wdist, axis=None)
 print('... Done.\nSaving...', flush=True)
 np.savez_compressed('output/vpxd_hist', hist=hist, bin_edges=bin_edges,
                     wavg=wavg, wvar=wvar, wskew=wskew, wkurt=wkurt)
