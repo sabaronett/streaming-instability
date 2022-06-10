0a581d5eedd641a9db4453c51be7e22209d49d4a
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-06-10 09:28:27 -0700

    Return absolute max value

--- unstrat/scripts/vpmax.py
+++ unstrat/scripts/vpmax.py
@@ -2,8 +2,8 @@
 #=======================================================================
 # vpmax.py
 #
-# Prints the maximum radial and vertical particle velocities among
-# particle output (.pout) data.
+# Prints the absolute maximum radial and vertical particle velocities
+# among particle output (.pout) data.
 #
 # Author: Stanley A. Baronett
 # Created: 2022-06-09
@@ -19,7 +19,7 @@ cases = ['AB', 'BA']
 Pis = ['0.01', '0.02', '0.05', '0.10']
 res = 2048
 
-print('Run\t\t\tv_{p,x,max} [H/T]\t\t\tv_{p,z,max} [H/T]')
+print('\nRun\t\t\tv_{p,x,max} [H/T]\t\t\tv_{p,z,max} [H/T]')
 print('=======================================================================')
 
 for case in cases:
@@ -27,6 +27,6 @@ for case in cases:
         run = f'{case:s}/{Pi:s}'
         output = '../'+run+f'/{res:n}/dat/SI.pout.00001.dat'
         time, pdata = athena_read.particles(output)
-        vpxmax, vpzmax = np.amax(pdata['vpx']),  np.amax(pdata['vpz'])
+        vpxmax, vpzmax = np.amax(abs(pdata['vpx'])), np.amax(abs(pdata['vpz']))
 
         print(f'{run:s}\t\t{vpxmax:.16e}\t\t{vpzmax:.16e}')
