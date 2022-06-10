fe2bbb1090a1c8d80242102952dc6a372d2abb64
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-06-10 07:53:40 -0700

    Print results to stdout

--- unstrat/scripts/vpmax.py
+++ unstrat/scripts/vpmax.py
@@ -2,11 +2,12 @@
 #=======================================================================
 # vpmax.py
 #
-# Returns the maximum radial and vertical particle velocities among
+# Prints the maximum radial and vertical particle velocities among
 # particle output (.pout) data.
 #
 # Author: Stanley A. Baronett
 # Created: 2022-06-09
+# Updated: 2022-06-10
 #=======================================================================
 
 import sys
@@ -17,29 +18,15 @@ import numpy as np
 cases = ['AB', 'BA']
 Pis = ['0.01', '0.02', '0.05', '0.10']
 res = 2048
-dirs = ['vpx', 'vpz']
-tab = [['Run', 'v_p,x,max [H/T]', 'v_p,z,max [H/T]']]
+
+print('Run\t\tv_{p,x,max} [H/T]\t\tv_{p,z,max} [H/T]')
+print('=======================================================================')
 
 for case in cases:
     for Pi in Pis:
         run = f'{case:s}/{Pi:s}'
         output = '../'+run+f'/{res:n}/dat/SI.pout.00001.dat'
-        print(f'Loading {run:s} particle data...')
         time, pdata = athena_read.particles(output)
-        row = [run, None, None]
+        vpxmax, vpzmax = np.amax(pdata['vpx']),  np.amax(pdata['vpz'])
 
-        for i in range(2):
-            dir = dirs[i]
-            print(f'  Finding the maximum {dir:s}...')
-            vpmax = np.amax(pdata[dir])
-            row[i+1] = f'{vpmax:0.16e}'
-
-        print(f'  Done.')
-        tab.append(row)
-
-fname = 'vpmaxs.txt'
-print(f'Saving to {fname:s}...')
-np.savetxt(fname, tab)
-print(f'Done.')
-
-unstrat/AB/0.01/2048/athdf
+        print(f'{run:s}\t\t{vpxmax:.16e}\t\t{vpzmax:.16e}')
