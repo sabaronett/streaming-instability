f5c577b1654e9aad52a451afee7bec804ceee343
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-08-02 09:43:18 -0700

    Don't normalize to epsilon; minor revs.

--- unstrat/scripts/cpdd.py
+++ unstrat/scripts/cpdd.py
@@ -8,7 +8,7 @@
 #
 # Author: Stanley A. Baronett
 # Created: 2022-02-08
-# Last Modified: 2022-03-12
+# Updated: 2022-08-02
 #==============================================================================
 import sys
 sys.path.insert(0, '/home6/sbaronet/athena-dust/vis/python')
@@ -17,30 +17,32 @@ import numpy as np
 from pathlib import Path
 
 # Collect Athena++ inputs, outputs, and sim constants
-t_sat = float(sys.argv[1])                   # code unit [T]
+t_sat = float(sys.argv[1])
 athinput = athena_read.athinput('athinput.si')
-epsilon = athinput['problem']['epsilon']     # avg. BG dust/gas ρ-ratio
-dt = athinput['output2']['dt']               # time between rhop outputs
-i_sat = int(t_sat / dt)                      # output index of sat. state
+dt = athinput['output2']['dt']
+i_sat = int(t_sat / dt)
 outputs = sorted(list(Path('athdf').glob(athinput["job"]["problem_id"]+
                                          '.out2.*.athdf')))
-sat_outputs = outputs[i_sat:]                # slice saturated state
-rhops = []                                   # sorted dust density snapshots
+sat_outputs = outputs[i_sat:]
+rhops = []
 
 print(f'Compiling data...', flush=True)
 
-for i,output in enumerate(sat_outputs):
+for i, output in enumerate(sat_outputs):
     data = athena_read.athdf(output)
-    rhops.append(np.sort(data['rhop'], axis=None))
+    sort = np.sort(data['rhop'], axis=None)
+    rhops.append(sort)
     print('  {:.2%}'.format(i/len(sat_outputs)), flush=True)
 
 print('  100%\nComputing statistical quantities...', flush=True)
-rhops = np.asarray(rhops)/epsilon            # convert for plot
+rhops = np.asarray(rhops)
 mins = np.amin(rhops, axis=0)
 maxs = np.amax(rhops, axis=0)
 avgs = np.average(rhops, axis=0)
-stds = np.exp(np.std(np.log(rhops), axis=0)) # computed in log space
-cdf = np.linspace(1, 0 , avgs.size, endpoint=False)
+log = np.log(rhops)
+std_log = np.std(log, axis=0)
+stds = np.exp(std_log)
+cdf = np.linspace(1, 0, avgs.size, endpoint=False)
 
 np.savez_compressed('output/cpdd', cdf=cdf, mins=mins, maxs=maxs, avgs=avgs,
                     stds=stds)
