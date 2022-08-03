f5c7acaf91bca9a99215c6683731a06bef17388e
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-08-03 15:08:33 -0700

    Add cbar to ea. plot; reformat subplots

--- unstrat/scripts/snapshots/snapshots.py
+++ unstrat/scripts/snapshots/snapshots.py
@@ -8,7 +8,7 @@
 #
 # Author: Stanley A. Baronett
 # Created: 2022-07-25
-# Updated: 2022-08-02
+# Updated: 2022-08-03
 #==============================================================================
 import sys
 sys.path.insert(0, '/home6/sbaronet/athena-dust/vis/python')
@@ -19,13 +19,13 @@ import matplotlib.colors as colors
 import matplotlib.ticker as ticker
 import numpy as np
 
-fig, axs = plt.subplots(2, 4, figsize=(8, 3.2), dpi=200)
+mpl.rcParams["axes.formatter.offset_threshold"] = 2
+fig, axs = plt.subplots(2, 4, sharex=True, sharey=True, figsize=(8, 5))
 workdir = '../..'
 case = 'AB'
-Pis = ['0.10', '0.05', '0.02', '0.01']
+Pis = ['0.01', '0.02', '0.05', '0.10']
 res = '2048'
 vmin_p, vmax_p = 1e-1, 1e1 # default for AB (CPDD > 85%; CPDD < 5%)
-vmin_g, vmax_g = 0, 0
 
 # Check for and override with user-passed arguments
 if len(sys.argv) > 1:
@@ -45,14 +45,19 @@ for i, Pi in enumerate(Pis):
     xf, zf = data['x1f']/H_g, data['x2f']/H_g
     t = data['Time']
     clip = np.clip(data['rhop'][0], vmin_p, vmax_p)
-    if i == 0:
-        vmin_g = np.amin(data['rho'][0])
-        vmax_g = np.amax(data['rho'][0])
-    j = len(Pis) - 1 - i
-    rhops = axs[0][j].pcolormesh(xf, zf, clip, norm=colors.LogNorm(), cmap='plasma')
-    rhogs = axs[1][j].pcolormesh(xf, zf, data['rho'][0], vmin=vmin_g, vmax=vmax_g)
-    axs[0][j].set(title=f'$\Pi={Pi:s}$', aspect='equal')
-    axs[1][j].set(xlabel=r'$x$ / $H_\mathrm{g}$', aspect='equal')
+    rhops = axs[0][i].pcolormesh(xf, zf, clip, norm=colors.LogNorm(), cmap='plasma')
+    rhogs = axs[1][i].pcolormesh(xf, zf, data['rho'][0])
+
+    # Add and format dust color bars, titles, and x-axis labels
+    cb_rhop = fig.colorbar(rhops, ax=axs[0][i], location='top')
+    axs[0][i].set_title(f'$\Pi={Pi:s}$', pad=40)
+    axs[0][i].set(aspect='equal')
+    axs[1][i].set(xlabel=r'$x$ / $H_\mathrm{g}$', aspect='equal')
+
+    # Add and format gas color bars
+    formatter = ticker.ScalarFormatter(useMathText=True)
+    formatter.set_powerlimits((-1, 1))
+    cb_rhog = fig.colorbar(rhogs, ax=axs[1][i], location='top', format=formatter)
 
 for ax in axs.flat:
     ax.label_outer()
@@ -61,16 +66,11 @@ for ax in axs.flat:
     ax.tick_params(axis='both', which='both', top=True, right=True)
     ax.tick_params(axis='x', labelrotation=45)
 
-# Add and format color bars
-mpl.rcParams["axes.formatter.offset_threshold"] = 2
-formatter = ticker.ScalarFormatter(useMathText=True)
-formatter.set_powerlimits((-1, 1))
-cb_rhop = fig.colorbar(rhops, ax=axs.flat[:4], pad=0.03)
-cb_rhog = fig.colorbar(rhogs, ax=axs.flat[4:], pad=0.03, format=formatter)
-cb_rhop.set_label(r'$\rho_\mathrm{p}$ / $\rho_\mathrm{g,0}$')
-cb_rhog.set_label(r'$\rho_\mathrm{g}$ / $\rho_\mathrm{g,0}$')
-
 # Format and save figure
+axs[0][0].text(-0.6, 1.31, r'$\rho_\mathrm{p}$ / $\rho_\mathrm{g,0}$', ha='left',
+               va='top', transform=axs[0][0].transAxes)
+axs[1][0].text(-0.6, 1.31, r'$\rho_\mathrm{g}$ / $\rho_\mathrm{g,0}$', ha='left',
+               va='top', transform=axs[1][0].transAxes)
 axs[0][0].set(ylabel=r'$z$ / $H_\mathrm{g}$')
 axs[1][0].set(ylabel=r'$z$ / $H_\mathrm{g}$')
 plt.savefig(f'figs/{case}-{res}_snaps.png', dpi=1000, bbox_inches='tight',
