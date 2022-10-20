6bf896114b0a29f8d419490350d244ec8e9be637
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2022-10-20 11:36:08 -0700

    Fix r' pole to ACF peak; use log-spaced bins

--- unstrat/scripts/autocorrelations/AB_avgRs_rad-prof.py
+++ unstrat/scripts/autocorrelations/AB_avgRs_rad-prof.py
@@ -2,41 +2,28 @@
 #==============================================================================
 # AB_avgRs_rad-prof.py
 #
-# Plot azimuthally-averaged radial profiles of the time-averaged normalized
-# autocorrelations of snapshots of the dust and gas density fields across a
-# range of radial pressure gradients for case AB.
+# Plot with logarithmically-spaced bins azimuthally-averaged radial profiles of
+# the time-averaged normalized autocorrelations of snapshots of the dust and
+# gas density fields across a range of radial pressure gradients for case AB.
 #
 # Author: Stanley A. Baronett
 # Created: 2022-10-09
-# Updated: 2022-10-10
+# Updated: 2022-10-20
 #==============================================================================
 import sys
 sys.path.insert(0, '/home6/sbaronet/athena-dust/vis/python')
 import athena_read
 import matplotlib.pyplot as plt
-import matplotlib.colors as colors
 import numpy as np
 from pathlib import Path
-from scipy import fftpack
+from scipy import fftpack, stats
 
-def norms (xf, zf):
-    rf = np.empty((len(zf), len(xf)))
-    for i,z in enumerate(zf):
-        for j,x in enumerate(zf):
-            rf[i][j] = np.sqrt(x**2 + z**2)
-    return rf
-
-def rad_prof(data, center):
-    """
-    https://stackoverflow.com/a/21242776/13149558
-    """
-    y, x = np.indices(data.shape)
-    r = np.sqrt((x - center[0])**2 + (y - center[1])**2)
-    r = r.astype(int)
-    tbin = np.bincount(r.ravel(), data.ravel())
-    nr = np.bincount(r.ravel())
-    profile = tbin / nr
-    return profile
+def norms(xv, zv, pole):
+    rv = np.zeros((len(zv), len(xv)))
+    for i, z in enumerate(zv):
+        for j, x in enumerate(xv):
+            rv[i][j] = np.sqrt((x - pole[0])**2 + (z - pole[1])**2)
+    return rv
 
 fig, axs = plt.subplots(2, sharex=True, figsize=(3.15, 4))
 workdir = '../..'
@@ -52,48 +39,77 @@ if len(sys.argv) > 1:
     t_sat = float(sys.argv[2])
 
 for i, Pi in enumerate(Pis):
-    # Collect parameters and plot densities
+    # Collect parameters
     print(f'{case}/{Pi[0]}: Processing...', flush=True)
     path = f'{workdir}/{case}/{Pi[0]}/{res}'
     athinput = athena_read.athinput(f'{path}/athinput.si')
-    outputs = sorted(list(Path(f'{path}/athdf').glob(\
+    outputs = sorted(list(Path(f'{path}/athdf').glob(
         athinput['job']['problem_id']+'.out1.*.athdf')))
     dt = athinput['output1']['dt']
-    i_sat  = int(t_sat/dt)
+    i_sat  = t_sat//dt
     outputs = outputs[i_sat:]
     c_s = athinput['hydro']['iso_sound_speed']
     etar = float(Pi[0])*c_s
     data = athena_read.athdf(outputs[0])
-    xf, zf = data['x1f']/etar, data['x2f']/etar
-    center = int(len(xf)/2)
-    xf, zf = np.delete(xf, center), np.delete(zf, center)
-    rf = norms(xf, zf)
-    rs = rad_prof(rf, [center, center])
+    xv, zv = data['x1v'], data['x2v']
+    left, right = xv[-1]/res, 1.32*xv[-1]
+    x0, z0 = len(xv)//2, len(zv)//2
+    pole = (xv[x0], zv[z0])
+    rv = norms(xv, zv, pole).ravel()
+    indices = np.where(rv > xv[-1])[0]
+    indices = np.append(indices, np.where(rv == 0)[0])
+    rv = np.delete(rv, indices)
+    r0 = xv[x0]
+    base = np.sqrt(2)
+    leftmost_edge = r0/np.sqrt(base)
+    num = int(np.sqrt(res))
+    bin_edges = leftmost_edge*np.logspace(0, num, num=(num + 1), base=base)
+    rvs = np.empty((len(outputs), res, res))
     Rps = np.empty((len(outputs), res, res))
     Rgs = np.empty((len(outputs), res, res))
 
     for j, output in enumerate(outputs):
         data = athena_read.athdf(output)
+        rvs[j] = rv
+        # Process dust
         ft = fftpack.fft2(data['rhop'][0])
         ac = fftpack.ifft2(ft*np.conjugate(ft)).real
         norm = ac/ac[0][0]
         shift = fftpack.fftshift(norm)
-        log = np.log10(shift)
-        Rps[j] = log
+        log = np.log10(shift).ravel()
+        Rps[j] = np.delete(log, indices)
+        # Process gas
         ft = fftpack.fft2(data['rho'][0])
         ac = fftpack.ifft2(ft*np.conjugate(ft)).real
         norm = ac/ac[0][0]
         shift = fftpack.fftshift(norm)
-        offset = (shift - 1)*1e8
-        Rgs[j] = offset
+        offset = (shift.ravel() - 1)*1e11
+        Rgs[j] = np.delete(offset, indices)
         print(f'\t{j/len(outputs):.0%}', flush=True)
 
-    avgRp = np.average(Rps, axis=0)
-    avgRg = np.average(Rgs, axis=0)
-    Rp_prof = rad_prof(avgRp, [center, center])
-    Rg_prof = rad_prof(avgRg, [center, center])
-    axs[0].plot(rs, Rp_prof, color=Pi[1], label=Pi[0])
-    axs[1].plot(rs, Rg_prof, color=Pi[1])
+    # Bin dust
+    dust_means, bin_edges, binnumnber = stats.binned_statistic(rvs.ravel(),
+        Rps.ravel(), statistic='mean', bins=bin_edges)
+    dust_stds, bin_edges, binnumnber = stats.binned_statistic(rvs.ravel(),
+        Rps.ravel(), statistic='std', bins=bin_edges)
+    dust_highs = dust_means + dust_stds
+    dust_lows = dust_means - dust_stds
+    # Bin gas
+    gas_means, bin_edges, binnumnber = stats.binned_statistic(rvs.ravel(),
+        Rgs.ravel(), statistic='mean', bins=bin_edges)
+    gas_stds, bin_edges, binnumnber = stats.binned_statistic(rvs.ravel(),
+        Rgs.ravel(), statistic='std', bins=bin_edges)
+    gas_highs = gas_means + gas_stds
+    gas_lows = gas_means - gas_stds
+    # Plot results
+    axs[0].stairs(dust_means, bin_edges, baseline=float('-inf'),
+                  color=Pi[1], lw=1.5, label=Pi[0])
+    axs[0].stairs(dust_highs, bin_edges, baseline=dust_lows, fill=True,
+                color=Pi[1], alpha=0.2)
+    axs[1].stairs(gas_means, bin_edges, baseline=float('-inf'),
+                  color=Pi[1], lw=1.5)
+    axs[1].stairs(gas_highs, bin_edges, baseline=gas_lows, fill=True,
+                  color=Pi[1], alpha=0.2)
     print(f'\tdone.', flush=True)
 
 for ax in axs.flat:
@@ -104,9 +120,9 @@ for ax in axs.flat:
 
 # Format and save figure
 axs[0].legend(title=r'$\Pi$')
-axs[0].set(ylabel=r'$\log(\int\mathrm{R}_{\rho_\mathrm{p}\rho_\mathrm{p}}\mathrm{d}r)$')
-axs[1].set(xlabel=r'$x/(\eta r)$', xscale='log',
-           ylabel=r'$\int\mathrm{R}_{\rho_\mathrm{g}\rho_\mathrm{g}}\mathrm{d}r\times10^{-8}+1$')
+axs[0].set(ylabel=r'$\log\left(\int_0^{2\pi}\mathrm{R}_{\rho_\mathrm{p}\rho_\mathrm{p}}\mathrm{d}\phi\right)$')
+axs[1].set(xlim=(left, right), xscale='log', yscale='symlog',
+           ylabel=r'$\left(\int_0^{2\pi}\mathrm{R}_{\rho_\mathrm{g}\rho_\mathrm{g}}\mathrm{d}\phi-1\right)\times10^{11}$')
 plt.subplots_adjust(hspace=0)
-plt.savefig(f'figs/{case}_autocorrelations.png', dpi=1000,
-            bbox_inches='tight', pad_inches=0.01)
+plt.savefig(f'figs/{case}_avgRs_rad-prof.pdf', bbox_inches='tight',
+            pad_inches=0.01)
