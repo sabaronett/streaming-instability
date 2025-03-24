97525d27da9ca5c64ea4a5c280fe3da28010fdb3
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2025-03-23 17:03:09 -0700

    Create npz directory as needed

--- unstrat/scripts/rhopmax.py
+++ unstrat/scripts/rhopmax.py
@@ -6,12 +6,13 @@
 #
 # Author: Stanley A. Baronett
 # Created: 2023-11-05
-# Updated: 2023-11-06
+# Updated: 2025-03-23
 #==============================================================================
 import sys
 sys.path.insert(0, '/home6/sbaronet/athena-dust/vis/python')
 import athena_read
 import numpy as np
+import os
 from pathlib import Path
 
 # Collect Athena++ inputs, outputs, and sim constants
@@ -29,4 +30,6 @@ for i, output in enumerate(outputs):
     print('  {:.2%}'.format(i/len(outputs)), flush=True)
 
 print(f'  Done.\nSaving results...', flush=True)
+if not os.path.exists('npz'):
+    os.makedirs('npz')  # create directory as needed
 np.savez_compressed('npz/time_series_rhopmax', rhopmax=rhopmax, t=t)
